b2935c2f139fc0c87d416cb22470ae05c99c9fe6
haiderali3010@gmail.com <haiderali3010@gmail.com>
2014-10-20 01:49:13 +0500

    Code Upgrade

--- js/main.js
+++ js/main.js
@@ -1,22 +1,49 @@
 angular.module('todoApp', [])
-    .controller('ChatOneController', function($scope, $rootScope) {
-        $rootScope.chat = [];
-        $scope.text = '';
-
-        $scope.addItem = function(){
-            if(!$scope.text || $scope.text.length >= 121) return;
-
-            $scope.chat.push({text : $scope.text, sender : 'user1'});
-            $scope.text = '';
-        };
+    .run(function($rootScope){
+        $rootScope.$on('emmitChatOneMessage', function(event, message){
+            $rootScope.$broadcast('broadcastChatOneMessage', message)
+        });
+        $rootScope.$on('emmitChatTwoMessage', function(event, message){
+            $rootScope.$broadcast('broadcastChatTwoMessage', message)
+        });
     })
-    .controller('ChatTwoController', function($scope) {
+//////controller one
+    .controller('ChatOneController', function($scope) {
+        $scope.chat = [];
         $scope.text = '';
 
-        $scope.addItem = function(){
-            if(!$scope.text || $scope.text.length >= 121) return;
-
-            $scope.chat.push({text : $scope.text, sender : 'user2'});
-            $scope.text = '';
+        $scope.emmitMessageChatOne = function(){
+            $scope.$emit('emmitChatOneMessage', {text: $scope.text});
         };
+        $scope.$on('broadcastChatOneMessage', function(event, message){
+            if(!message.text || message.text.length >= 121) return;
+
+            $scope.chat.push({text : message.text, sender : 'user1'});
+            $scope.text = '';
+        });
+        $scope.$on('broadcastChatTwoMessage', function(event, message){
+            if(!message.text || message.text.length >= 121) return;
+
+            $scope.chat.push({text : message.text, sender : 'user2'});
+        });
+    })
+//////controller two
+    .controller('ChatTwoController', function($scope) {
+        $scope.chat = [];
+        $scope.text = '';
+
+        $scope.emmitMessageChatTwo = function(){
+            $scope.$emit('emmitChatTwoMessage', {text: $scope.text});
+        };
+        $scope.$on('broadcastChatOneMessage', function(event, message){
+            if(!message.text || message.text.length >= 121) return;
+
+            $scope.chat.push({text : message.text, sender : 'user1'});
+        });
+        $scope.$on('broadcastChatTwoMessage', function(event, message){
+            if(!message.text || message.text.length >= 121) return;
+
+            $scope.chat.push({text : message.text, sender : 'user2'});
+            $scope.text = '';
+        });
     });
